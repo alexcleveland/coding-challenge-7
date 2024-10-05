f6eb344c8067f6b2322e29900da74d89ec539587
Alex Cleveland <acleveland2@usf.edu>
2024-10-05 18:55:01 -0400

    create recursive function to calculate department salary

--- department_salary.js
+++ department_salary.js
@@ -47,4 +47,28 @@ const company ={
 ]
 };
  
+// Task 2 Create a Recursive Function to Calculate Total Salary for a Department
+
+function calculateDepartmentSalary(department) {
+    let totalSalary = 0;
+    for (const employee of department.employees){
+        totalSalary += employee.salary;
+
+        if(employee.subordinates.length > 0){
+        totalSalary += calculateDepartmentSalary(employee.subordinates);
+    }
+}
+return totalSalary;
+}
+
+function calculateDepartmentSalary(subordinates) {
+    let total = 0;
+    for (const subordinates of subordinates){
+        total += subordinate.salary;
+        if (subordinate.subordinates.length > 0){
+            total += calculateDepartmentSalary(subordinate.subordinates);
+        }
+    }
+    return total;
+}
 
